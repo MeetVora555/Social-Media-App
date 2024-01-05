f00fac38e4d5cdf5a9b41e7758fc8490cd3302ff
MeetVora555 <meetvora555@gmail.com>
2024-01-05 10:44:12 +0530

    Final Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.makeramen:roundedimageview:2.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.0.0")
+    implementation("com.facebook.shimmer:shimmer:0.4.0")
 }
